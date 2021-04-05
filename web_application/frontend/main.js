0e942dd0df5dd249ebc3876a6f9e3bb78155af5c
Rutar Andriy <RutarAndriy@gmail.com>
2021-04-05 21:19:39 +0300

    Update main.js

--- web_application/frontend/main.js
+++ web_application/frontend/main.js
@@ -2,10 +2,10 @@
 // Підключення модулів ...........................................................................
 
 // Підключаємо express - веб фреймворк
-const express = require('express');
+const express = require("express");
 
 // Підключаємо path - модуль для роботи із шляхами
-const path = require('path');
+const path = require("path");
 
 // ...............................................................................................
 // Створення необхідних змінних ..................................................................
@@ -17,7 +17,7 @@ const exp = express();
 const PORT = process.env.PORT || 8080;
 
 // Шлях до директорії проекту
-const dir_proj = path.join(__dirname, '/../../');
+const dir_proj = path.join(__dirname, "/../../");
 
 // Шлях до директорії фронтенду
 const dir_front = __dirname;
@@ -32,43 +32,49 @@ const dir_views = path.join(dir_front, "/views");
 exp.use(express.static(dir_proj));
 
 // Задаємо шаблонізатор, який буде використовуватися для відображення веб-сторінок
-exp.set('view engine', 'ejs');
+exp.set("view engine", "ejs");
 
 // Задаємо шлях до view-елементів
-exp.set('views', dir_views);
+exp.set("views", dir_views);
 
 // ...............................................................................................
 // Налаштовуємо маршрутизацію
 
 // ... для головної сторінки
-exp.get(['/', '/index'], function (request, response) {
-  response.render('pages/index', { title: 'Головна сторінка' });
+exp.get(["/", "/index"], function (request, response) {
+  response.render("pages/index", { title: "Головна сторінка",
+                                   page_id: "0" });
 });
 
 // ... для сторінки "Лікарні"
-exp.get('/hospitals', function (request, response) {
-  response.render('pages/hospitals', { title: 'Лікарні' });
+exp.get("/hospitals", function (request, response) {
+  response.render("pages/hospitals", { title: "Лікарні",
+                                       page_id: "1" });
 });
 
 // ... для сторінки "Лікарі"
-exp.get('/doctors', function (request, response) {
-  response.render('pages/doctors', { title: 'Лікарі' });
+exp.get("/doctors", function (request, response) {
+  response.render("pages/doctors", { title: "Лікарі",
+                                     page_id: "2" });
 });
 
 // ... для сторінки "Пацієнти"
-exp.get('/patients', function (request, response) {
-  response.render('pages/patients', { title: 'Пацієнти' });
+exp.get("/patients", function (request, response) {
+  response.render("pages/patients", { title: "Пацієнти",
+                                      page_id: "3" });
 });
 
 // ... для сторінки "Виписані пацієнти"
-exp.get('/discharged', function (request, response) {
-  response.render('pages/discharged', { title: 'Виписані пацієнти' });
+exp.get("/discharged", function (request, response) {
+  response.render("pages/discharged", { title: "Виписані пацієнти",
+                                        page_id: "4" });
 });
 
 // ... для сторінки 404 - "Сторінку не знайдено"
 exp.use(function (request, response) {
   response.status(404);
-  response.render('pages/404', { title: '404'});
+  response.render("pages/404", { title: "404",
+                                 page_id: "-1"});
 });
 
 // ...............................................................................................
