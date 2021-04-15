b2ef2e90775350727ae87166ab7021c912b326e2
Rutar Andriy <RutarAndriy@gmail.com>
2021-04-15 13:06:24 +0300

    Update basic.js

---
+++ web_application/frontend/js/basic.js
@@ -0,0 +1,29 @@
+// Зберігання даних у localStorage
+function save_data (collection) {
+
+}
+
+// Завантаження даних з localStorage
+function load_data (collection) {
+
+}
+
+// Створення нового елемента
+function create_element() {
+   console.log(`Create: ${location.pathname}`);
+}
+
+// Пошук існуючого елемента
+function find_element (input) {
+   console.log(`Find: ${location.pathname}`);
+}
+
+// Редагування існуючого елемента
+function edit_element (button) {
+   console.log(`Edit: ${button}`);
+}
+
+// Видалення існуючого елемента
+function delete_element (button) {
+   console.log(`Delete: ${button}`);
+}
